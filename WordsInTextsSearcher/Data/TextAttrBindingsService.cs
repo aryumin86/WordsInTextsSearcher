54f4d089ac0eeacdf0d54733f446433371cb55f5
Алексей Рюмин <aryumin@aryumin.ru>
2021-07-04 20:13:25 +0300

    added staff for text attr bindings

---
+++ WordsInTextsSearcher/Data/TextAttrBindingsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WordsInTextsSearcher.Repos;
+
+namespace WordsInTextsSearcher.Data
+{
+    public class TextAttrBindingsService
+    {
+        private ITextAttrBindingsRepo _textAttrBindingsRepo;
+        public TextAttrBindingsService(ITextAttrBindingsRepo textAttrBindingsRepo)
+        {
+            _textAttrBindingsRepo = textAttrBindingsRepo;
+        }
+    }
+}
---
+++ WordsInTextsSearcher/Repos/ITextAttrBindingsRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordsInTextsSearcher.Repos
+{
+    public interface ITextAttrBindingsRepo
+    {
+    }
+}
