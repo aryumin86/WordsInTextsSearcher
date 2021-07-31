e6069b5435d9bf08d16efce08bbf9b092c581328
Алексей Рюмин <aryumin@aryumin.ru>
2021-07-31 11:28:29 +0300

    added simple contextual words extractor

---
+++ TextsProc.LIba/Parsing/DumbStaff/NaiveWordsContextGetter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextsProc.LIba.Parsing.DumbStaff
+{
+    /// <summary>
+    /// Gets n words before and after the main one.
+    /// </summary>
+    public class NaiveWordsContextGetter
+    {
+        private readonly IEnumerable<char> _delimeters = new List<char>
+        {
+            ' ', ',', '.', '/', '?','!','@','\"', '#', '$','%','^', '&', '*', '(', ')', '-','_',
+            '=', '+', ';','[', ']', '\\', '|'
+        };
+
+        /// <summary>
+        /// Extracts context words within n words around
+        /// </summary>
+        /// <param name="mainWord"></param>
+        /// <param name="input"></param>
+        /// <param name="n">n words to the left and to the right of the main one</param>
+        /// <param name="predicate">predicate to filter results</param>
+        /// <returns></returns>
+        public IEnumerable<string> GetWords(string input, string mainWord, int n, Func<string, bool> predicate)
+        {
+            var res = new List<string>();
+            var allWords = input.Split(_delimeters.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string currentWord = string.Empty;
+            for(int i = 0; i < allWords.Length; i++)
+            {
+                currentWord = allWords[i];
+                if (currentWord.ToLowerInvariant() != mainWord.ToLowerInvariant())
+                    continue;
+
+                int paces = n + 1;
+                for(var j = i; j < allWords.Length && paces > 0; j++, paces--)
+                {
+                    res.Add(allWords[j]);
+                }
+
+                paces = n + 1;
+                for (var j = i; j >= 0 && paces > 0; j--, paces--)
+                {
+                    res.Add(allWords[j]);
+                }
+            }
+
+            return res.Where( w=> w.ToLowerInvariant() != mainWord.ToLowerInvariant()).Distinct();
+        }
+    }
+}
---
+++ TextsProc.LIbaTests/Parsing/DumbStaff/NaiveWordsContextGetterTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextsProc.LIba.Parsing.DumbStaff;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TextsProc.LIba.Parsing.DumbStaff.Tests
+{
+    [TestClass()]
+    public class NaiveWordsContextGetterTests
+    {
+        [TestMethod()]
+        public void GetWordsTest()
+        {
+            var contextGetter = new NaiveWordsContextGetter();
+            Func<string, bool> filter = (string word) => word.Length > 4;
+
+            string input = "xxx xxx aaa bbbb доктрина ccc aaa bb fff";
+            var words = contextGetter.GetWords(input, "доктрина", 2, filter);
+            Assert.IsTrue(words.Contains("bbbb") && words.Contains("aaa") &&
+                words.Contains("ccc") 
+                && !words.Contains("xxx") && !words.Contains("bb") && !words.Contains("fff"));
+        }
+    }
+}
